e4d8039ae6fcf4dcd28ea714e4c129997f940386
Bojan Ramšak <bojan.ramsak@rtvslo.si>
2022-06-09 09:06:41 +0200

    Change version number

--- lib/cherryEpg.pm
+++ lib/cherryEpg.pm
@@ -23,7 +23,7 @@ use IPC::ConcurrencyLimit;
 use Fcntl qw/:flock O_WRONLY O_CREAT O_EXCL/;
 use open qw ( :std :encoding(UTF-8));
 
-our $VERSION = '2.1.24';
+our $VERSION = '2.1.25';
 
 with('MooX::Singleton');
 
